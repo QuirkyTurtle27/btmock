3e9749442cee12c3a4b205b1c226e96d8783524a
MateiGrigoras <matei.grigoras@gs1.ro>
2025-08-01 18:09:48 +0300

    Start

---
+++ src/main/java/ro/gs1/btmock/PaymentResultBean.java
@@ -0,0 +1,16 @@
+package ro.gs1.btmock;
+
+import java.io.Serializable;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+
+@Named
+@ViewScoped
+public class PaymentResultBean implements Serializable{
+
+   private static final long serialVersionUID = 2455747052202290966L;
+
+
+}
